aa6d35b9b6dda2e27db122dda7e238c37b7fdc16
mariam <mariam.avagyan25@gmail.com>
2023-12-05 14:25:25 -0500

    load mnist

--- MNIST.py
+++ MNIST.py
@@ -1,1 +1,19 @@
-import numpy as np
+import torch
+import torchvision
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+BATCH_SIZE = 32
+
+# preprocessing
+# normalize = transforms.Normalize(mean=[.5], std=[.5])
+transform = transforms.Compose([transforms.ToTensor()])
+
+# download and load the data
+train_dataset = torchvision.datasets.MNIST(root='./mnist/', train=True, transform=transform, download=True)
+test_dataset = torchvision.datasets.MNIST(root='./mnist/', train=False, transform=transform, download=False)
+
+# encapsulate them into dataloader form
+train_loader = data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+test_loader = data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False,drop_last=True)
+
