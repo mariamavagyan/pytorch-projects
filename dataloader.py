d0b5c644b6d0d1eeba2dd0070aeca011b4354632
mariam <mariam.avagyan25@gmail.com>
2023-12-06 14:28:45 -0500

    created dataloader

---
+++ dataloader.py
@@ -0,0 +1,45 @@
+# import torch
+# import torch.nn as nn
+import torch.utils.data as Data
+import torchvision
+
+import os
+
+class MNISTDataLoader:
+
+    DOWNLOAD_MNIST = False # why is this here? is it global?
+
+    # If MNIST directory does not exist: not(os.path.exists('./mnist'))
+    # or is empty: os.listdir('./mnist') -- this lists the contents of the dir
+    if not(os.path.exists('./mnist')) or not os.listdir('./mnist'):
+        DOWNLOAD_MNIST = True
+    
+    def __init__(self, batch_size = 32, num_workers = 2):
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.transform = torchvision.transforms.ToTensor()
+
+    
+    def _load_train(self):
+
+        self.train_data = torchvision.datasets.MNIST(root='./mnist',
+                                                     train = True,
+                                                     transform=self.transform,
+                                                     download=self.DOWNLOAD_MNIST)
+        
+        self.train_loader = Data.DataLoader(dataset = self.train_data,
+                                            batch_size=self.batch_size,
+                                            shuffle=True,
+                                            num_workers=self.num_workers)
+        
+    def _load_test(self):
+
+        self.test_data = torchvision.datasets.MNIST(root='./mnist',
+                                                    train = False,
+                                                    transform=self.transform,
+                                                    download=self.DOWNLOAD_MNIST)
+        
+        self.test_loader = Data.DataLoader(dataset = self.test_data,
+                                           batch_size=self.batch_size,
+                                           shuffle = True,
+                                           num_workers=self.num_workers)
